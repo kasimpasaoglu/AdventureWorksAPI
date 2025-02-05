9c527f4bd11e3f0716ee327bcdce6ba26298faae
Kasim Pasaoglu <kasimpasaoglu@windowslive.com>
2025-02-06 00:52:44 +0300

    fix-register

--- Models/Register.cs
+++ Models/Register.cs
@@ -3,34 +3,34 @@ public class RegisterDTO
     public string PersonType { get; set; } = "IN";
     public bool NameStyle { get; set; } = false;
     public string? Title { get; set; }
-    required public string FirstName { get; set; }
+    public string FirstName { get; set; }
     public string? MiddleName { get; set; }
-    required public string LastName { get; set; }
-    required public string EmailAddress1 { get; set; }
-    required public string Password { get; set; }
-    required public string PasswordSalt { get; set; }
-    public int EmailPromotion { get; set; }
-    required public int AddressTypeId { get; set; }
-    required public string AddressLine1 { get; set; }
-    required public string? AddressLine2 { get; set; }
-    required public string City { get; set; }
-    required public int StateProvinceId { get; set; }
-    required public string PostalCode { get; set; }
+    public string LastName { get; set; }
+    public string EmailAddress1 { get; set; }
+    public string Password { get; set; }
+    public string PasswordSalt { get; set; }
+    public int EmailPromotion { get; set; } = 0;
+    public int AddressTypeId { get; set; }
+    public string AddressLine1 { get; set; }
+    public string? AddressLine2 { get; set; }
+    public string City { get; set; }
+    public int StateProvinceId { get; set; } = 1;
+    public string PostalCode { get; set; }
 }
 public class RegisterVM
 {
     public string? Title { get; set; }
-    required public string FirstName { get; set; }
+    public string FirstName { get; set; }
     public string? MiddleName { get; set; }
-    required public string LastName { get; set; }
-    required public string EmailAddress1 { get; set; }
-    required public string Password { get; set; }
-    public int EmailPromotion { get; set; }
-    required public int AddressTypeId { get; set; }
-    required public string AddressLine1 { get; set; }
-    required public string? AddressLine2 { get; set; }
-    required public string City { get; set; }
-    required public int StateProvinceId { get; set; }
-    required public string PostalCode { get; set; }
+    public string LastName { get; set; }
+    public string EmailAddress1 { get; set; }
+    public string Password { get; set; }
+    public int EmailPromotion { get; set; } = 0;
+    public int AddressTypeId { get; set; }
+    public string AddressLine1 { get; set; }
+    public string? AddressLine2 { get; set; }
+    public string City { get; set; }
+    public int StateProvinceId { get; set; } = 1;
+    public string PostalCode { get; set; }
 
 }
